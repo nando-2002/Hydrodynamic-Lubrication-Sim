fixing z derivative being 0

2DReynoldsSolver.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from sympy import *
 
 from matplotlib import cm
 
@@ -20,34 +21,55 @@ p_new = np.zeros([nx, nz])
 
 #init of film thickness values and derivatives
 
-A = -0.04*10**(-3)
-B = 0.02*10**(-3)
+M = -0.04*10**(-3)
+N = 0.02*10**(-3)
+O = 0.01*10**(-3)
+def h(i, j):
+    i, j = symbols('i j')
+    f = (M*i + N + O*(j + (nz*dz/2))**2)
+    ff = f.subs([(i,i),(j,j)])
+    return f
 
-def h(i):
-    return (A*i + B)
+def dhdx(i, j):
+    i, j = symbols('i j')
+    f = (M*i + N + O*(j + (nz*dz/2))**2)
+    ff = f.subs([(i,i),(j,j)])
+    return diff(ff, i)
 
-def dhdx(i):
-    return A
+def dhdz(i, j):
+    i, j = symbols('i j')
+    f = (M*i + N + O*(j + (nz*dz/2))**2)
+    ff = f.subs([(i,i),(j,j)])
+    return diff(ff, j)
 
-def dhdz(i):
-    return 0
+def dh3dx(i, j):
+    i, j = symbols('i j')
+    f = (M*i + N + O*(j + (nz*dz/2))**2)
+    ff = f.subs([(i,i),(j,j)])
+    return diff(ff**3, i)
 
-def dh3dx(i):
-    return 3*A*h(i)
-
-def dh3dz(i):
-    return 0
+def dh3dz(i, j):
+    i, j = symbols('i j')
+    f = (M*i + N + O*(j + (nz*dz/2))**2)
+    ff = f.subs([(i,i),(j,j)])
+    return diff(ff**3, j)
 
 #init of fluid values and boundary conditions
 
 mu = 10**(-3)#Pa s
 U = 20#m/s
 
-for a in range(5000):
+for a in range(500):
     for u in range(1, nx - 1):
         for v in range(1, nz - 1):
             #print(u, v, a)
-            p_new[u,v] = (1/((-2/(6*mu*U))*((dh3dx(u)/dx) + (dh3dz(v)/dz)))*(dhdx(u) - (1/(6*mu*u))*((dh3dx(u)*(p_old[u+1,v]+p_old[u-1,v]))+(dh3dz(v)*(p_old[u,v+1]+p_old[u,v-1])))))
+            A = 6*mu*U*dx
+            B = 6*mu*U*dz
+            p_new[u,v] = ( (1/(-2*((dh3dx(u, v)/A) + (dh3dz(u, v)/B)))) 
+                *(dhdx(u, v) - (dh3dx(u, v)/A)*(p_old[u + 1, v] + p_old[u - 1, v])
+                          - (dh3dz(u, v)/B)*(p_old[u, v + 1] + p_old[u, v - 1])
+                    )
+                )
     p_old = p_new
     
 fig, ax = plt.subplots(subplot_kw = {"projection":"3d"})